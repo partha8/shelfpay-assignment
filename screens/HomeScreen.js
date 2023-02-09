0e49a504ed85c8614e43b97a1e4e6d43312e3a04
Partha Pratim Sarma <64675650+partha8@users.noreply.github.com>
2023-02-09 21:52:22 +0530

    feat: new group btn

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,22 +1,35 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useStateContext } from "../context/StateProvider";
+import { AntDesign } from "@expo/vector-icons";
 
 const HomeScreen = () => {
-  const { contacts } = useStateContext();
   const navigation = useNavigation();
- 
+
   return (
-    <View>
-      <TouchableOpacity>
-        <Text onPress={() => navigation.navigate("Contacts")}>
-          Go to contacts
-        </Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.newGroupBtn}
+        onPress={() => navigation.navigate("Contacts")}
+      >
+        <AntDesign name="pluscircle" size={34} color="blue" />
       </TouchableOpacity>
-      <Text></Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    height: "100%",
+  },
+  newGroupBtn: {
+    position: "absolute",
+    top: "90%",
+    right: "5%",
+    elevation: 5,
+    shadowColor: "black",
+  },
+});
 
 export default HomeScreen;
